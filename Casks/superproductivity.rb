e90e3f3610fc5dea55b44b427e7a32014f95a631
Johannes Millan <johannesjo@users.noreply.github.com>
2020-05-15 16:23:26 +0200

    Update superproductivity.rb
    Set version to latest

--- Casks/superproductivity.rb
+++ Casks/superproductivity.rb
@@ -1,5 +1,5 @@
 cask 'superproductivity' do
-  version '5.1.4'
+  version :latest
   sha256 'bf677582b4acdc6409af081bbee4b53e9afab81a28772675d097e621520fa2b0'
 
   # github.com/johannesjo/super-productivity/ was verified as official when first introduced to the cask
